a89221e0796ae6ec5256c75ddd350589da8ea867
Brian Olingay <brianolingay@gmail.com>
2021-04-28 20:31:37 +0800

    Auth schemas

--- backend/app/graphql/backend_schema.rb
+++ backend/app/graphql/backend_schema.rb
@@ -1,9 +1,16 @@
 class BackendSchema < GraphQL::Schema
+  use GraphqlDevise::SchemaPlugin.new(
+    query: Types::QueryType,
+    mutation: Types::MutationType,
+    resource_loaders: [
+      GraphqlDevise::ResourceLoader.new('User')
+    ]
+  )
   mutation(Types::MutationType)
   query(Types::QueryType)
 
   # Union and Interface Resolution
-  def self.resolve_type(abstract_type, obj, ctx)
+  def self.resolve_type(_abstract_type, _obj, _ctx)
     # TODO: Implement this function
     # to return the correct object type for `obj`
     raise(GraphQL::RequiredImplementationMissingError)
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
-  end
+  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql' if Rails.env.development?
 
-  post "/graphql", to: "graphql#execute"
+  post '/graphql', to: 'graphql#execute'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
